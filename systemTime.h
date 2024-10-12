901983c49597d830dcd8b89521cad4df4d7eda37
le-it-24 <128759370+le-it-24@users.noreply.github.com>
2024-10-12 06:00:32 -0700

    first commit

---
+++ systemTime.h
@@ -0,0 +1,26 @@
+
+
+
+
+/***SYSTEM TIME STRUCTURE***/
+struct time
+{
+    uint16_t milliseconds;
+    uint8_t seconds;
+    uint8_t minutes;
+    uint8_t hour;
+    uint8_t day;
+    uint8_t month;
+    uint16_t year;
+    int leapYear;
+};
+
+
+
+
+/***SYSTEM TIMING VARIABLES***/
+struct time sysTime;
+
+
+
+
