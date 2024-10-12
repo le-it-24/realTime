6ecffa296ca1476cb070ea29534a29ca7c1e2e82
le-it-24 <128759370+le-it-24@users.noreply.github.com>
2024-10-12 08:11:23 -0700

    RTC app integration.

---
+++ realTime_config.h
@@ -0,0 +1,34 @@
+/* 
+ * File:   realTime_config.h
+ * Author: Leonard
+ *
+ * Created on October 12, 2024, 6:03 AM
+ */
+
+#ifndef REALTIME_CONFIG_H
+#define	REALTIME_CONFIG_H
+#endif
+
+
+
+
+/***REALTIME METHOD PARAMETER DEFINITIONS***/
+#define MILLIS          0
+#define SEC             1
+
+
+
+
+/***CALANDER DATA DEFINITIONS***/
+#define JANUARY         1
+#define FEBRUARY        2
+#define MARCH           3
+#define APRIL           4
+#define MAY             5
+#define JUNE            6
+#define JULY            7
+#define AUGUST          8
+#define SEPTEMBER       9
+#define OCTOBER         10
+#define NOVEMBER        11
+#define DECEMBER        12
